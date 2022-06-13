cac243a2c31305937f4bba9a69f29d9e9a91834f
TheJeds <diegogalaxy15@gmail.com>
2022-06-12 22:58:07 -0500

    add Announcement Component

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Announcement() {
+    return (
+        <div className='h-[30px] bg-sky-500 text-white flex items-center justify-center text-sm font-medium'>
+            Super Deal! Free Shipping on Orders Over $50
+        </div>
+    )
+};
+
+export default Announcement;
